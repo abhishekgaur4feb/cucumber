3fa9b55b88ac2844158179edccd85294c82d2f96
abhishekgaur4feb <35129583+abhishekgaur4feb@users.noreply.github.com>
2018-01-05 16:26:07 +0530

    comment Minor changes

--- src/test/java/Page_Object/Filter_By_Diamond_page.java
+++ src/test/java/Page_Object/Filter_By_Diamond_page.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+//Added by Abhishek Gaur
+
 	public class Filter_By_Diamond_page {
 		public static WebDriver driver;
 		
