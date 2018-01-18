87564c7667be569488f624864c01c4435317bb36
abhishekgaur4feb <35129583+abhishekgaur4feb@users.noreply.github.com>
2018-01-18 16:53:11 +0530

    email changes

---
+++ src/test/java/Step_Defination/EmailAttachmentSender.java
@@ -0,0 +1,114 @@
+package Step_Defination;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+ 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+ 
+public class EmailAttachmentSender {
+	
+    public static void sendEmailWithAttachments(String host, String port,
+            final String userName, final String password, String toAddress,
+            String subject, String message, String[] attachFiles)
+            throws AddressException, MessagingException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.user", userName);
+        properties.put("mail.password", password);
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+ 
+        // creates message part
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(message, "text/html");
+ 
+        // creates multi-part
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+ 
+        // adds attachments
+        if (attachFiles != null && attachFiles.length > 0) {
+            for (String filePath : attachFiles) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+ 
+                try {
+                    attachPart.attachFile(filePath);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+ 
+                multipart.addBodyPart(attachPart);
+            }
+        }
+ 
+        // sets the multi-part as e-mail's content
+        msg.setContent(multipart);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+ 
+    }
+ 
+    /**
+     * Test sending e-mail with attachments
+     * @return 
+     */
+    public void testmail() {
+        // SMTP info
+    	 String host = "smtp.gmail.com";
+         String port = "587";
+         String mailFrom = "abhishekgaur4feb@gmail.com";
+         String password = "Sparkle7@";
+  
+         // message info
+         String mailTo = "abhishekgaur054@gmail.com";
+         String subject = "Rare Carat Search Time Duration";
+         String message = "Hi all,"+"<p/>"+"	Diamond Search Time is taking More than 15 seconds.Kindly Check the Reason Behind."+ "<p/>"+"Please find attached logs for reference."+ "<p/>"+"Regards"+ "<p/>"+"Abhishek Gaur";
+         
+         
+         // attachments
+         String[] attachFiles = new String[1];
+         attachFiles[0] = "cucumber/log/testlog.log";
+         //attachFiles[1] = "e:/Test/Music.mp3";
+        // attachFiles[2] = "e:/Test/Video.mp4";
+  
+        try {
+            sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                subject, message, attachFiles);
+            System.out.println("Email sent.");
+        } catch (Exception ex) {
+            System.out.println("Could not send email.");
+            ex.printStackTrace();
+        }
+    }
+}
